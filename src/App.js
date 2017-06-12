7a14e32f2c5e01007336d85d7407bfdcfc16486e
Pablo Ferrer <dtecnologia@dreamersoft.com>
2017-06-12 15:41:57 -0500

    build a good structure for the project and implement the home page including react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className='container'>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route render={function () {
+              return <p>NOT FOUND!</p>
+            }} />
+          </Switch>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="title">Duckr!</p>
+        <p className="slogan">The real time, cloud based, modular, scalable growth hack, social platform.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
